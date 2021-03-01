6c217938b6c107887151f596cf093875cb6c97b6
Alhuin <julien.janinr@protonmail.com>
2021-03-01 09:36:18 +0100

    menu in profile

--- project/__init__.py
+++ project/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
+
 from project.models import User
 
 
--- project/main.py
+++ project/main.py
@@ -1,8 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 from flask_login import login_required, current_user
+from .api.spotify import app_authorization, user_authorization, get_profile_data, get_playlist_data, get_album_data
 from . import db
 
 main = Blueprint('main', __name__)
+
+
+@main.route('/spotify')
+def spotify_connect():
+    # Authorization
+    auth_url = app_authorization()
+    return redirect(auth_url)
+
+
+@main.route('/callback/q')
+def spotify_callback():
+    authorization_header = user_authorization()
+
+    profile_data = get_profile_data(authorization_header)
+    print("PROFILE DATA")
+    print(profile_data)
+
+    playlist_data = get_playlist_data(authorization_header, profile_data)
+    print("PLAYLIST DATA")
+    print(playlist_data)
+
+    artist_data = get_album_data(authorization_header, profile_data, 50, 0)
+    print("ARTIST DATA")
+    print(artist_data)
+
+    return "callback"
 
 
 @main.route('/')
